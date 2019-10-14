84edf085538e151f0b99f762842e7b298926cf14
Soumava Ghosh <ghosh.soumava4@gmail.com>
2019-10-14 18:47:39 +0530

    Add files via upload
    adding variational encoder

--- autoencoder_model.py
+++ autoencoder_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
+from torch.distributions import Normal
 
 class auto(nn.Module):
 
@@ -64,4 +65,65 @@ class sparse_auto(nn.Module):
 
         return loss_val + kl
 
+class var_auto(nn.Module):
 
+    def __init__(self, inp_size, dist='gaussian', hidden_size=64):
+        super(var_auto, self).__init__()
+
+        self.inp_size = inp_size
+        self.hidden_size = hidden_size
+        self.enc1 = nn.Linear(self.inp_size, 256, bias = True)
+        self.enc2 = nn.Linear(256, 128, bias = True)
+        self.mean_net = nn.Linear(128, self.hidden_size, bias=True)
+        self.std_net = nn.Linear(128, self.hidden_size, bias=True)
+        self.dec1 = nn.Linear(self.hidden_size, 128, bias=True)
+        self.dec2 = nn.Linear(128, 256, bias=True)
+        self.dec3 = nn.Linear(256, self.inp_size, bias=True)
+        self.dist = dist
+        self.loss1 = nn.MSELoss()
+        self.loss2 = nn.BCELoss()
+
+        self.init_emb()
+
+    def init_emb(self):
+
+        initrange = 0.01
+        self.enc1.weight.data.uniform_(-initrange, initrange)
+        self.enc2.weight.data.uniform_(-initrange, initrange)
+        self.mean_net.weight.data.uniform_(-initrange, initrange)
+        self.std_net.weight.data.uniform_(-initrange, initrange)
+        self.dec1.weight.data.uniform_(-initrange, initrange)
+        self.dec2.weight.data.uniform_(-initrange, initrange)
+        self.dec3.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input):
+        h = self.enc1(input)
+        h = F.relu(h)
+        h = self.enc2(h)
+        h = F.relu(h)
+        mean = self.mean_net(h)
+        std = F.softplus(self.std_net(h))+0.0000001
+
+        sample = Normal(0, 1).sample((input.shape[0], self.hidden_size))
+        sample = sample * std + mean
+        out = self.dec1(sample)
+        out = F.relu(out)
+        out = self.dec2(out)
+        out = F.relu(out)
+        out = self.dec3(out)
+        if self.dist == 'bernoulli':
+            out = F.sigmoid(out)
+        else:
+            out = F.relu(out)
+        loss_v, kl = self.calc_loss(input, std, mean, out)
+
+        return loss_v, kl, out, sample
+
+    def calc_loss(self, input, std, mean, out):
+        if self.dist == 'gaussian':
+            loss_val = torch.mean(self.loss1(out, input))
+        elif self.dist == 'bernoulli':
+            loss_val = torch.mean(self.loss2(out, input))
+        kl = torch.mean(torch.pow(mean, 2)/2 + torch.pow(std, 2)/2 - torch.log(std) -0.5)
+
+        return loss_val, kl
--- main.py
+++ main.py
@@ -1,29 +1,79 @@
 import pandas as pd
-from autoencoder_model import sparse_auto, auto
+from autoencoder_model import sparse_auto, auto, var_auto
 from sklearn.utils import shuffle
 import torch
 from torch import optim
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from sklearn.manifold import TSNE
 
-data = pd.read_csv()
+data = pd.read_csv('mnist_train.csv', header = 0)
+data = np.array(data[list(data)[1:]])
 
-model = sparse_auto(data.shape[0])
+data = torch.Tensor(data).type(torch.FloatTensor)/256.0
+N = data.size()[0]
+
+model = var_auto(data.shape[1], 'gaussian', 64)
 model.train()
 opt = optim.Adam(model.parameters(), lr = 0.005)
 
-batch_size = 64
+batch_size = 512
 losses = []
 
-epoch = 100000
+epoch = 10000
 
-for _ in range(epoch):
+for i in range(epoch):
     opt.zero_grad()
 
-    data = shuffle(data)
-    train_data = data[:batch_size]
+    batch_indices = torch.LongTensor(np.random.randint(0, N, size=128))
+    train_data = torch.index_select(data, 0, batch_indices)
 
-    loss, _, _ = model(train_data)
+    loss, kl , out , sample = model(train_data)
+    print(str(i)+' - '+ str(loss.data)+' - '+str(kl.data))
+    loss = loss
     loss.backward()
-    losses.append(loss)
+    losses.append(loss.data.numpy())
 
     opt.step()
-    print(loss)
+
+print('done')
+
+def disp(i):
+    sample = t_data[i]
+    sample = torch.Tensor(sample.unsqueeze(0))
+    loss, kl , out , sample= model(sample)
+    out = out*256
+    out = out.type(torch.LongTensor).clamp(0,256)
+    out = out.data.numpy().reshape(28,28)
+    plt.imshow(out)
+
+test = pd.read_csv('mnist_train.csv', header = 0)
+t_data = np.array(test[list(test)[1:]])
+
+t_data = torch.Tensor(t_data).type(torch.FloatTensor)/256.0
+loss, kl , out, sample = model(t_data)
+
+sample = sample.data.numpy()
+sample_tsne = TSNE(n_components=2, verbose = 5).fit_transform(sample)
+
+i = 1500
+print(test.iloc[i]['label'])
+plt.imshow(t_data[i].reshape(28, 28))
+disp(i)
+
+n = 10
+colors = cm.rainbow(np.linspace(0, 1, n))
+fig, ax = plt.subplots()
+
+for i in range(n):
+#    t_data = np.array(test[test['label']==i][list(test)[1:]])
+#    t_data = torch.Tensor(t_data).type(torch.FloatTensor)/256.0
+#    loss, kl , out, sample = model(t_data)
+    ind = np.array(test[test['label']==i].index)
+    sample1 = sample_tsne[ind,:]
+    
+    ax.scatter(sample1[:,0], sample1[:,1], color=colors[i], label = i)
+    
+ax.legend()
+plt.show()
